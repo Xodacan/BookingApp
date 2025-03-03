d30f4cee92af9a8327b13d1e43db6e948dd2beea
pragun <prgun383@gmail.com>
2025-03-03 23:33:16 +1000

    Database/migrations added

---
+++ Migrations/20250303133129_initialCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TennisCourtAPI.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courts",
+                columns: table => new
+                {
+                    CourtId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CourtName = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courts", x => x.CourtId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    BookingId = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TimeSlot = table.Column<string>(type: "TEXT", nullable: true),
+                    CourtId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.BookingId);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Courts_CourtId",
+                        column: x => x.CourtId,
+                        principalTable: "Courts",
+                        principalColumn: "CourtId");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courts",
+                columns: new[] { "CourtId", "Address", "CourtName", "ImageUrl" },
+                values: new object[] { 1, "add1", "Court 1", null });
+
+            migrationBuilder.InsertData(
+                table: "Courts",
+                columns: new[] { "CourtId", "Address", "CourtName", "ImageUrl" },
+                values: new object[] { 2, "add2", "Court 2", null });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_CourtId",
+                table: "Bookings",
+                column: "CourtId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Courts");
+        }
+    }
+}
